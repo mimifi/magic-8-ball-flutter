60a91e55a488b9f3abab35127bf34dce9cbbc387
Mantab Tadayon <mt.tadayon@gmail.com>
2019-12-15 14:14:36 +0100

    Create the app with functionality on tapping on the image to change it.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,64 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  int changeBallImageNumber() {
+    return Random().nextInt(4) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          ballNumber = changeBallImageNumber();
+        });
+      },
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('images/ball$ballNumber.png'),
+              Container(
+                height: 20.0,
+              ),
+              Text(
+                'Please tap on the image.',
+                style: TextStyle(color: Colors.lightBlue[900], fontSize: 24.0),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
